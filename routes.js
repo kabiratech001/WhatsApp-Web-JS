4fbafc6c5236b6fd7ea4e0d9e3ee7c26c0df5087
kabiratech001 <161125959+kabiratech001@users.noreply.github.com>
2025-04-22 11:04:36 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -8,86 +8,57 @@ const upload = multer({ storage: multer.memoryStorage() });
 const base64ImageContent = require("./scripts/base64ImageContent");
 
 module.exports = function (app, client) {
-  // Route untuk mengecek apakah server berjalan
   app.get("/", (req, res) => {
-    res.send("Server berjalan!");
+    res.send("Server is running!");
   });
 
-  // Route untuk mengirim pesan teks ke id yang diberikan
-app.post("/send-plaintext", verifyKey, async (req, res) => {
-  const { message, id } = req.body;
-
-  // Check if message is defined and is an object
-  if (!message || typeof message !== "object" || Object.keys(message).length === 0 || !id) {
-    return res.status(400).send("Bad Request: Message and ID are required and message must be a non-empty object!");
-  }
-
-  try {
-    await txtContent(client, message, id);
-    const name = await getChatName(client, id);
-    res.send(`Pesan berhasil dikirim ke ${name}!`);
-  } catch (error) {
-    res.status(500).send(`Failed to send message: ${error.message}`);
-  }
-
+  app.post("/send-plaintext", verifyKey, async (req, res) => {
+    const { message, id } = req.body;
+    if (!message || typeof message !== "object" || Object.keys(message).length === 0 || !id) {
+      return res.status(400).send("Bad Request: Message and ID are required and message must be a non-empty object!");
+    }
     try {
       await txtContent(client, message, id);
       const name = await getChatName(client, id);
-      res.send(`Pesan berhasil dikirim ke ${name}!`);
+      res.send(`Message successfully sent to ${name}!`);
     } catch (error) {
       res.status(500).send(`Failed to send message: ${error}`);
     }
   });
 
-  // Route untuk mengirim pesan berupa file ke id yang diberikan
-  app.post(
-    "/send-file",
-    verifyKey,
-    upload.array("attachment"),
-    async (req, res) => {
-      const { caption, id } = req.body;
-      const attachmentFiles = req.files;
-
-      if (!attachmentFiles || attachmentFiles.length === 0 || !id) {
-        return res
-          .status(400)
-          .send("Bad Request: Attachments and ID are required!");
-      }
-
-      try {
-        await jsonContent(client, caption, attachmentFiles, id);
-        const name = await getChatName(client, id);
-        res.send(`Pesan berhasil dikirim ke ${name}!`);
-      } catch (error) {
-        res.status(500).send(`Failed to send message: ${error}`);
-      }
+  app.post("/send-file", verifyKey, upload.array("attachment"), async (req, res) => {
+    const { caption, id } = req.body;
+    const attachmentFiles = req.files;
+    if (!attachmentFiles || attachmentFiles.length === 0 || !id) {
+      return res.status(400).send("Bad Request: Attachments and ID are required!");
     }
-  );
+    try {
+      await jsonContent(client, caption, attachmentFiles, id);
+      const name = await getChatName(client, id);
+      res.send(`Message successfully sent to ${name}!`);
+    } catch (error) {
+      res.status(500).send(`Failed to send message: ${error}`);
+    }
+  });
 
-  // Route untuk mendapatkan id dari group berdasarkan nama group
   app.get("/get-group-id", verifyKey, async (req, res) => {
     const groupName = req.header("groupName");
     if (!groupName)
       return res.status(400).send("Bad Request: Group Name is required!");
-
     const groupId = await getGroupID(client, groupName);
     if (!groupId) return res.status(404).send("Group not found!");
-
     res.send(groupId);
   });
 
-  // Route untuk mengirim gambar berupa base64 ke id yang diberikan
   app.post("/send-base64-image", verifyKey, async (req, res) => {
     const { caption, id, images: base64Images } = req.body;
-
     if (!base64Images || base64Images.length === 0 || !id) {
       return res.status(400).send("Bad Request: Images and ID are required!");
     }
-
     try {
       await base64ImageContent(client, caption, base64Images, id);
       const name = await getChatName(client, id);
-      res.send(`Pesan berhasil dikirim ke ${name}!`);
+      res.send(`Message successfully sent to ${name}!`);
     } catch (error) {
       res.status(500).send(`Failed to send message: ${error}`);
     }
